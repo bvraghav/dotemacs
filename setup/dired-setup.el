cc12c0eae103171cdc367af453361b112899f457
B.V. Raghav <bvraghav@gmail.com>
2018-07-25 07:59:08 +0530

    Function and keymap to hide dotfiles from dired buffer.

--- setup/dired-setup.el
+++ setup/dired-setup.el
@@ -1,5 +1,15 @@
 ;; Dired
 
+;; Functions
+;; -----------------------------------
+
+(defun bvr-dired-kill-dotfiles ()
+  (interactive)
+  (dired-mark-files-regexp "\\..*")
+  (dired-do-kill-lines))
+
+;; -----------------------------------
+
 ;; Dired Mode archive handler
 (eval-after-load "dired-aux"
   '(add-to-list 'dired-compress-file-suffixes 
@@ -8,5 +18,6 @@
 ;; Dired Mode Map
 
 (define-key dired-mode-map (kbd "C-c C-n") 'dired-narrow)
+(define-key dired-mode-map (kbd "C-c .") #'bvr-dired-kill-dotfiles)
 
 (provide 'dired-setup)
