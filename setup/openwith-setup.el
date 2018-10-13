2952d37a9b3dc764c079536cdd8d08d56db6c621
B.V. Raghav <bvraghav@gmail.com>
2018-10-14 04:50:37 +0530

    Feh to open image files.

--- setup/openwith-setup.el
+++ setup/openwith-setup.el
@@ -1,5 +1,10 @@
 (require 'openwith)
 (openwith-mode t)
-(setq openwith-associations '(("\\.pdf\\'" "evince" (file))))
+(setq openwith-associations '(("\\.pdf\\'" "evince" (file))
+			      ("\\.png\\'" "feh" (file))
+			      ("\\.jpe?g\\'" "feh" (file))
+			      ("\\.gif\\'" "feh" (file))
+			      ("\\.tiff?\\'" "feh" (file))
+			      ))
 
 (provide 'openwith-setup)
