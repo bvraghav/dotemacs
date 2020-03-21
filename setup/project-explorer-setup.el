7437e6795a49d25c154bc7d8e366460bb1287ef2
B.V. Raghav <bvraghav@gmail.com>
2020-03-21 07:42:46 +0530

    Disable linum in project explorer.

--- setup/project-explorer-setup.el
+++ setup/project-explorer-setup.el
@@ -3,7 +3,12 @@
   (global-set-key (kbd "C-z C-s") #'project-explorer-open)
   (global-set-key (kbd "C-z s") #'project-explorer-open))
 
+(defun disable-linum ()
+  (linum-mode -1))
+
 (when (require 'project-explorer nil 'noerror)
-    (set-pe-keybindings))
+  (set-pe-keybindings)
+  (add-hook 'project-explorer-mode-hook
+	    #'disable-linum))
 
 (provide 'project-explorer-setup)
