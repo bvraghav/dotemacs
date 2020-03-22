61ad9a921275b962a0d00ecd4268db45c9fb5405
B.V. Raghav <bvraghav@gmail.com>
2020-03-22 08:45:02 +0530

    Add random slugs.

--- setup/slug.el
+++ setup/slug.el
@@ -8,4 +8,40 @@
       (slug (car kill-ring))
     (slug (completing read "String for slug: " kill-ring nil t (car kill-ring)))))
 
+
+(defun slug-random-string (N alphabet)
+  (let (str)
+    (dotimes (i N str)
+      (setq str (concat str (string (elt alphabet (random (length alphabet)))))))))
+
+(defun slug-insert-random-string (N)
+  (interactive "P")
+      (let ((alphabet (concat "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                              "abcdefghijklmnopqrstuvwxyz"
+                              "0123456789"
+                              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                              "abcdefghijklmnopqrstuvwxyz"
+                              "0123456789"
+                              "!@#$%^-_=+{}[];:<>,.?")))
+        (insert (slug-random-string (or N 8) alphabet))))
+
+(defun slug-insert-random-string-from-alphabet (N alphabet)
+  (interactive (list (string-to-number (read-string "N:" nil nil "8"))
+                     (let ((alp (list (concat "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                              "abcdefghijklmnopqrstuvwxyz"
+                                              "0123456789"
+                                              "0123456789"
+                                              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                              "abcdefghijklmnopqrstuvwxyz"
+                                              "0123456789"
+                                              "0123456789"
+                                              "!@#$%^-_=+{}[];:<>,.?")
+                                      (concat "abcdefghijklmnopqrstuvwxyz"
+                                              "0123456789"
+                                              "0123456789"
+                                              "--"))))
+                       (read-string "alphabet: " nil nil alp))))
+  (insert (slug-random-string N alphabet)))
+  
+
 (provide 'slug)
