6148b599d278f226a76ed330a8958e6b2a8e2c8c
B.V. Raghav <bvraghav@gmail.com>
2020-05-04 11:19:34 +0530

    Enable ggtags for python.

--- setup/python-setup.el
+++ setup/python-setup.el
@@ -1,5 +1,13 @@
+;; Run ggtags if projectile root folder available
+;; ====================================================
+(defun bvr-run-ggtags-may-be ()
+  (interactive)
+  nil)
+
 (defun bvr-python-mode-hook ()
-  (customize-set-variable 'indent-tabs-mode nil))
+  (customize-set-variable 'indent-tabs-mode nil)
+  (ggtags-mode 1)
+  (projectile-mode 1))
 
 (add-hook 'python-mode-hook #'bvr-python-mode-hook)
 
