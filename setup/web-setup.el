42e00c3000399412edd77d60259be8d9122d4012
B.V. Raghav <bvraghav@gmail.com>
2020-09-22 03:35:30 +0530

    Add css-completion support for web-related modes.

--- setup/web-setup.el
+++ setup/web-setup.el
@@ -1,12 +1,16 @@
 ;; Php mode
 (require 'sgml-mode)
 
+;; CSS class completion
+(require 'css-completion-minor-mode)
+
 (defun bvr-php-indentation()
   "Indent php to 2 spaces"
   (interactive)
   (setq tab-width 2
 	c-basic-offset 2))
 (add-hook 'php-mode-hook #'bvr-php-indentation)
+(add-hook 'php-mode-hook #'css-completion-minor-mode)
 
 ;; Web mode
 (defun bvr-web-mode-indentation ()
@@ -15,6 +19,7 @@
   (setq tab-width 2
 	c-basic-offset 2))
 (add-hook 'web-mode-hook #'bvr-web-mode-indentation)
+(add-hook 'web-mode-hook #'css-completion-minor-mode)
 
 ;; Apache Indent Level
 (setq apache-indent-level 2)
@@ -22,5 +27,6 @@
 ;; Use mark multiple to edit sgml tag
 (define-key sgml-mode-map (kbd "C-c C-r") #'rename-sgml-tag)
 (define-key sgml-mode-map (kbd "C-c r") #'rename-sgml-tag)
+(add-hook 'sgml-mode-hook #'css-completion-minor-mode)
 
 (provide 'web-setup)
