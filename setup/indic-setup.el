9b816a494fe24a3199e632f62e544752326e8a79
B.V. Raghav <bvraghav@gmail.com>
2021-01-05 13:59:14 +0530

    Define a function for indic setup/

--- setup/indic-setup.el
+++ setup/indic-setup.el
@@ -1,19 +1,28 @@
-;; "Devanagari"  U+0900 - U+1097F
-(set-fontset-font "fontset-default"
-                  (cons (decode-char 'ucs #x0900)
-                        (decode-char 'ucs #x097f))
-                  "Eczar")
+(defun devanagari-setup ()
+  (interactive)
 
-;; "Devanagari Extended"  U+A8E0 - U+A8FF
-(set-fontset-font "fontset-default"
-                  (cons (decode-char 'ucs #xa8e0)
-                        (decode-char 'ucs #xa8ff))
-                  "Eczar")
+  ;; "Devanagari"  U+0900 - U+1097F
+  (set-fontset-font "fontset-default"
+                    (cons (decode-char 'ucs #x0900)
+                          (decode-char 'ucs #x097f))
+                    "Eczar")
 
-;; "Vedic Extensions" (Devanagari) U+1CD0 - U+1CFF
-(set-fontset-font "fontset-default"
-                  (cons (decode-char 'ucs #x1cd0)
-                        (decode-char 'ucs #x1cff))
-                  "Eczar")
+  ;; "Devanagari Extended"  U+A8E0 - U+A8FF
+  (set-fontset-font "fontset-default"
+                    (cons (decode-char 'ucs #xa8e0)
+                          (decode-char 'ucs #xa8ff))
+                    "Eczar")
+
+  ;; "Vedic Extensions" (Devanagari) U+1CD0 - U+1CFF
+  (set-fontset-font "fontset-default"
+                    (cons (decode-char 'ucs #x1cd0)
+                          (decode-char 'ucs #x1cff))
+                    "Eczar"))
+
+(defun indic-fonts-setup ()
+  (interactive)
+  (devanagari-setup))
+
+(indic-fonts-setup)
 
 (provide 'indic-setup)
