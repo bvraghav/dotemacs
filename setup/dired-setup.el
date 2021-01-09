e21c7f3222a65a8e5253b0e82ff27aa8aed7c259
B.V. Raghav <bvraghav@gmail.com>
2021-01-09 17:13:10 +0530

    Refactor dired setup with use-package syntax. Slightly more legible.

--- setup/dired-setup.el
+++ setup/dired-setup.el
@@ -1,52 +1,50 @@
 ;; Dired
 
-;; Functions
-;; -----------------------------------
+(use-package dired-x
+  :after dired-aux
 
-(defun bvr-dired-kill-dotfiles ()
-  (interactive)
-  (dired-mark-files-regexp "\\..*")
-  (dired-do-kill-lines))
+  :bind (:map dired-mode-map
+              ("C-c C-n" . dired-narrow)
+              ("C-c ."   . bvr-dired-kill-dot-files)
+              ("C-c C-d" . bvr-dired-desktop-revert)
+              ("C-c C-t" . bvr-dired-open-tmux-here))
+
+  :init
+  (defun bvr-dired-kill-dotfiles ()
+    (interactive)
+    (dired-mark-files-regexp "\\..*")
+    (dired-do-kill-lines))
 
 
-(defun bvr-dired-desktop-revert ()
-  (interactive)
-  (let* ((filename (dired-get-file-for-visit))
-	 (desktop-dirname (file-name-directory filename))
-	 (desktop-base-file-name (file-name-nondirectory filename)))
-    (desktop-revert)))
+  (defun bvr-dired-desktop-revert ()
+    (interactive)
+    (let* ((filename (dired-get-file-for-visit))
+	   (desktop-dirname (file-name-directory filename))
+	   (desktop-base-file-name (file-name-nondirectory filename)))
+      (desktop-revert)))
 
-(defun bvr-dired-open-tmux-here()
-  (interactive)
-  (let ((dirname (or (when (equal major-mode 'dired-mode)
-                       (bvr-dired-dwim-directory))
-                     default-directory)))
-    (async-shell-command
-     (format "xterm -e 'tmux new-session -c %s'" dirname))))
+  (defun bvr-dired-open-tmux-here()
+    (interactive)
+    (let ((dirname (or (when (equal major-mode 'dired-mode)
+                         (bvr-dired-dwim-directory))
+                       default-directory)))
+      (async-shell-command
+       (format "xterm -e 'tmux new-session -c %s'" dirname))))
 
-(defun bvr-dired-dwim-directory()
-  (let ((sel (dired-get-file-for-visit)))
-    (if (file-directory-p sel)
-        sel
-      (file-name-directory sel))))
+  (defun bvr-dired-dwim-directory()
+    (let ((sel (dired-get-file-for-visit)))
+      (if (file-directory-p sel)
+          sel
+        (file-name-directory sel))))
 
-;; -----------------------------------
+  :config
 
-;; Dired Mode archive handler
-(eval-after-load "dired-aux"
-  '(add-to-list 'dired-compress-file-suffixes 
-		'("\\.zip\\'" ".zip" "unzip")))
+  ;; Dired Mode archive handler
+  (add-to-list 'dired-compress-file-suffixes
+               '("\\.zip\\'" ".zip" "unzip"))
 
-;; Dired Mode Map
-
-(define-key dired-mode-map (kbd "C-c C-n") 'dired-narrow)
-(define-key dired-mode-map (kbd "C-c .") #'bvr-dired-kill-dotfiles)
-(define-key dired-mode-map (kbd "C-c C-d") #'bvr-dired-desktop-revert)
-(define-key dired-mode-map (kbd "C-c C-t") #'bvr-dired-open-tmux-here)
-(define-key dired-mode-map (kbd "C-c C-t") #'bvr-dired-open-tmux-here)
-
-;; Variables
-(setq  dired-isearch-filenames t
-       dired-listing-switches "-alh")
+  ;; Variables
+  (setq  dired-isearch-filenames t
+         dired-listing-switches "-alh")  )
 
 (provide 'dired-setup)
