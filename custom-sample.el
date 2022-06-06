15163fc3fb9d2dbce4561bda918c4ed8c80c1abd
B.V. Raghav <bvraghav@gmail.com>
2022-06-06 17:27:51 +0100

    Add sample custom.el file for faces.

---
+++ custom-sample.el
@@ -0,0 +1,9 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 96 :width normal :foundry "ADBO" :family "LIberation Mono"))))
+ '(col-highlight ((t (:background "#383838"))))
+ '(whitespace-hspace ((t (:background "#555555" :foreground "#aaaaaa"))))
+ '(whitespace-space ((t (:foreground "dark gray")))))
