c96f077f9be1a2d1aa8fe65b39edbffa394be1c0
B.V. Raghav <bvraghav@gmail.com>
2023-01-05 10:19:16 +0000

    Remap binding C-c C-c for send buffer instead of send file.

--- setup/gnuplot-setup.el
+++ setup/gnuplot-setup.el
@@ -1,6 +1,8 @@
 (use-package gnuplot
   :ensure t
   :mode ("\\.gnuplot\\'" . gnuplot-mode)
+  :bind (:map gnuplot-mode-map
+              ("C-c C-c" . gnuplot-send-buffer-to-gnuplot))
   )                                     ; The END
 
 (provide 'gnuplot-setup)
