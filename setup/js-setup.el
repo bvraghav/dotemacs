0888cbf6c53eabbecc155c1c3401b6de5de1086f
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2023-03-06 22:27:48 +0000

    Add some nvm related functions. To be documented later.

--- setup/js-setup.el
+++ setup/js-setup.el
@@ -1,7 +1,7 @@
 (use-package js2-mode
   :ensure t
 
-  :mode "\\.[cm]?js\\'"
+  :mode ("\\.[cm]?js\\'" "\\.json\\'")
 
   :bind (:map js2-mode-map
 	      ("C-x C-e"	. js-send-last-sexp)
@@ -25,25 +25,77 @@
         '("--experimental-json-modules" "--experimental-repl-await")))
 
 
-;; (require 'js2-mode)
+;; ----------------------------------------------------
+;; Setting up NVM Convenience functions
+;; ----------------------------------------------------
 
-;; ;; ES6 based javascript mode
-;; ;; (add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
-;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-;; (add-to-list 'auto-mode-alist '("\\.cjs\\'" . js2-mode))
-;; (add-to-list 'auto-mode-alist '("\\.mjs\\'" . js2-mode))
+;; Example defcustom
+;; -----------------------------------
+;; (defcustom frobnicate-automatically nil
+;;        "Non-nil means automatically frobnicate all buffers."
+;;        :type 'boolean
+;;        :require 'frobnicate-mode
+;;        :group 'frobnicate)
 
-;; (defun bvr/js2-key-bindings ()
-;;   (local-set-key (kbd "C-x C-e") 'js-send-last-sexp)
-;;   (local-set-key (kbd "C-M-x")   'js-send-last-sexp-and-go)
-;;   (local-set-key (kbd "C-c b")   'js-send-buffer)
-;;   (local-set-key (kbd "C-c C-b") 'js-send-buffer-and-go)
-;;   (local-set-key (kbd "C-c l")   'js-load-file-and-go)
-;;   (message "Added bvr/js2-key-bindings"))
+(defcustom bvr/shell-source-command "source"
+  "/bin/sh uses `\.' /bin/zsh uses `source'. Define based on
+source")
+(defcustom bvr/nvm-search-dirs-list
+  '("~/.nvm" "~/.config/nvm")
+  "List of dirs to search for nvm installation")
+(defcustom bvr/default-nvm-dir nil "NVM_DIR")
+(defcustom bvr/default-nodejs-repl-command nil "")
+(defcustom bvr/nvm-node-args nil "Node REPL args")
 
-;; (add-hook 'js2-mode-hook #'bvr/js2-key-bindings)
+(defun bvr/has-nvm-sh-p (dir)
+  "Search if `nvm.sh' is a top level member of dir in FS"
+  (file-exists-p (file-name-concat dir "nvm.sh")))
+(defun bvr/not-has-nvm-sh-p (dir)
+  "Negage #'bvr/has-nvm-sh-p"
+  (not (bvr/has-nvm-sh-p dir)))
+(defun bvr/find-nvm-dir (&optional path-list)
+  "Search for nvm installation in the given PATH-LIST, which is
+taken from 'bvr/nvm-search-dirs-list by default."
+  (let* ((path-list (or path-list bvr/nvm-search-dirs-list))
+         (found (-drop-while #'bvr/not-has-nvm-sh-p path-list)))
+    (and found (first found))))
+(defun bvr/nvm-dir ()
+  "Get of find NVM_DIR"
+  (expand-file-name
+   (or bvr/default-nvm-dir (bvr/find-nvm-dir))))
 
-;; (add-hook 'js2-mode-hook #'auto-revert-mode)
+(defun bvr/nvm-shell-cmd ()
+  "Get nvm shell command"
+  (or (getenv "NVM_DIR")
+      (setenv "NVM_DIR"))
+  (format "%s ${NVM_DIR}/nvm.sh ; nvm "
+          bvr/shell-source-command))
+
+(defun bvr/nvm-list-versions ()
+  "List acceptable versions for nvm"
+  (let* ((nvm-versions-cmd
+          (format "%s list --no-colors"
+                  (bvr/nvm-shell-cmd)))
+         (vraw (string-split (shell-command-to-string
+                              nvm-versions-cmd)
+                             "\n"))
+         (vtrim (mapcar #'string-trim vraw))
+         (vsplit (mapcar #'string-split vtrim))
+         (vdrop (mapcar (lambda (splits)
+                          (-drop-while (lambda (s) (string-prefix-p "-" s))
+                                       splits))
+                        vsplit))
+         (versions (mapcar #'first vdrop)))
+    versions))
+;; ----------------------------------------------------
+
+
+;; ----------------------------------------------------
+;; Indium Setup
+;; ----------------------------------------------------
+;; Indium does not work. Never add this.
+;;(use-package indium :ensure t)
+
 
 ;; (defun bvr/ts-setup ()
 ;;   (auto-revert-mode 1)
