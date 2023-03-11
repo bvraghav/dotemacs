ae4a88bf06e5594753ba73284f40cd001bb6b1fd
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2023-03-11 08:51:42 +0000

    Set indent level.

--- setup/js-setup.el
+++ setup/js-setup.el
@@ -26,7 +26,9 @@
 
 (use-package json-mode
   :ensure t
-  :mode "\\.json\\'")
+  :mode "\\.json\\'"
+  :config
+  (setq js-indent-level 2))
 
 
 ;; ----------------------------------------------------
