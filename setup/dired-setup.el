4826b2fb0f2ffdd4d071cc9bbed8b2ec9b75f80b
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2023-03-27 11:17:46 +0100

    Fix #2, Fix #5.

--- setup/dired-setup.el
+++ setup/dired-setup.el
@@ -1,4 +1,35 @@
-;; Dired
+;;; dired-setup.el --- Setup Dired Enhancements -*- lexical-binding: t -*-
+
+;; Author: B.V. Raghav
+;; Maintainer: B.V. Raghav
+;; Version: 0.1
+;; Package-Requires: ()
+;; Homepage: https://github.com/bvraghav/dotemacs.git
+;; Keywords: dired,dotemacs
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Setup Dired Enhancements
+
+;;; Code:
+
 
 (use-package dired-x
   :after dired-aux
@@ -47,4 +78,13 @@
   (setq  dired-isearch-filenames t
          dired-listing-switches "-alh"))
 
+(use-package dired-quick-sort
+  :ensure t
+  :config
+  (dired-quick-sort-setup))
+
+
+
 (provide 'dired-setup)
+
+;;; dired-setup.el ends here
