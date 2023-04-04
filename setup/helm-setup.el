2d7f7ca97e149fc666a64b78eadae8eb00525329
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2023-04-04 22:21:44 +0100

    Add header.

--- setup/helm-setup.el
+++ setup/helm-setup.el
@@ -1,3 +1,37 @@
+;;; helm-setup.el --- Setup Helm -*- lexical-binding: t -*-
+
+;; Author: B.V. Raghav
+;; Maintainer: B.V. Raghav
+;; Version: 0.1
+;; Package-Requires: nil
+;; Homepage: https://github.com/bvraghav/dotemacs
+;; Keywords: helm dotemacs
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Setup helm.  Taken from other sources, but
+;; attributed accordingly.
+
+;;; Code:
+
+
 ;;; init-helm.el --- My startup file for helm. -*- lexical-binding: t -*-
 ;;; Source URI: https://github.com/thierryvolpiatto/emacs-tv-config/blob/master/init-helm.el
 ;;; Code:
@@ -715,6 +749,9 @@ empty string to inhibit prefix"
 (add-to-list 'completion-ignored-extensions ".dbus/")
 (add-to-list 'completion-ignored-extensions "dconf/")
 
+;;; init-helm.el ends here
+
 (provide 'helm-setup)
 
-;;; init-helm.el ends here
+;;; helm-setup.el ends here
+
