4cb15a20a5a898a0806077d156fee6018032f9ef
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2023-12-12 19:21:45 +0530

    Add conda create commands for cpu.

---
+++ conda-create.zsh
@@ -0,0 +1,55 @@
+#! /bin/zsh
+
+# conda install --yes   \
+
+conda create -n emacs   \
+beautifulsoup4 \
+click		\
+curl		\
+dominate	\
+ffmpeg		\
+flask		\
+gflags		\
+glog		\
+gstreamer	\
+h5py		\
+imageio		\
+jedi		\
+jsonschema	\
+jupyter		\
+kornia		\
+markdown	\
+matplotlib	\
+mlflow		\
+ncurses		\
+nettle		\
+ninja		\
+numpy		\
+pandas		\
+pcre		\
+pillow		\
+pip		\
+prompt-toolkit	\
+protobuf	\
+python \
+querystring_parser \
+re2 \
+readline \
+regex \
+requests \
+scikit-image \
+scikit-learn \
+scipy \
+soupsieve \
+sqlite=3 \
+tk \
+tqdm \
+xz \
+yaml \
+
+conda activate emacs
+
+conda install --yes pytorch torchvision torchaudio cpuonly -c pytorch
+
+
+conda install --yes lightning -c conda-forge
