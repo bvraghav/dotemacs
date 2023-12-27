e02b0a7ea1744228d5068153bf578ef477af30d3
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2023-12-27 22:32:22 +0530

    Define highlight bg along with col-highlight bg.

--- custom-sample.el
+++ custom-sample.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :extend nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 96 :width normal :foundry "ADBO" :family "Liberation Mono"))))
- '(col-highlight ((t (:background "#383838"))))
+ '(col-highlight ((t (:background "#434343"))))
+ '(highlight ((t (:background "#434343"))))
  '(whitespace-hspace ((t (:background "#555555" :foreground "#aaaaaa"))))
  '(whitespace-space ((t (:foreground "dark gray")))))
