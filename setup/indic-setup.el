189275c991a0580531d3958ce3f3eb5b2ffbd05c
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2024-09-22 21:40:09 +0530

    Add tamil setup to indic fonts.

--- setup/indic-setup.el
+++ setup/indic-setup.el
@@ -19,9 +19,25 @@
                           (decode-char 'ucs #x1cff))
                     "Eczar"))
 
+(defun tamil-setup ()
+  (interactive)
+
+  ;; "Tamil" U+0B80 - U+0BFF
+  (set-fontset-font "fontset-default"
+                    (cons (decode-char 'ucs #x0b80)
+                          (decode-char 'ucs #x0bff))
+                    "Noto Sans Tamil")
+
+  ;; "Tamil Supplement" U+11FC0 - U+11FFF
+  (set-fontset-font "fontset-default"
+                    (cons (decode-char 'ucs #x11fc0)
+                          (decode-char 'ucs #x11fff))
+                    "Noto Sans Tamil"))
+
 (defun indic-fonts-setup ()
   (interactive)
   (devanagari-setup)
+  (tamil-setup)
   (define-key 'iso-transl-ctl-x-8-map "r" [?₹]))
 
 (indic-fonts-setup)
